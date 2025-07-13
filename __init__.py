16a722e52d83091130291b96a12ba38f4ef785b3
Giulia Sepeda <51463466+giuuusepeda@users.noreply.github.com>
2025-07-13 14:43:54 +0200

    Update __init__.py
    added smart_summary

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@ from .who_api import get_who_data
 from .cleaning import clean_column_names
 from .io_tools import save_to_csv
 from .visualization import plot_line
+from .eda_tools import smart_summary
 
 __all__ = [
     "get_who_data",
     "clean_column_names",
     "save_to_csv",
-    "plot_line"
+    "plot_line",
+    "smart_summary"
 ]
